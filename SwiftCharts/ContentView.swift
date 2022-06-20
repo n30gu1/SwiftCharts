fbb16d066744a4f5c3ce1678f5ea3ca9424452b2
n30gu1 <n@n30gu1.com>
2022-06-20 22:58:25 +0900

    Initial Commit

---
+++ SwiftCharts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftCharts
+//
+//  Created by Seongheon Park on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Seongheon Park on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
